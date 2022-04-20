ad67d0302a99821a06265cf871a0dffc7df8ea2e
TikiTikiPlus <71807594+TikiTikiPlus@users.noreply.github.com>
2022-04-21 10:56:33 +1200

    Add files via upload
    "faulty or statement"

--- state.java
+++ state.java
@@ -1,9 +1,13 @@
+package com.company;
+
 public class state {
     private char _symbol;
     private int _nextPhrase1;
     private int _nextPhrase2;
-    public state(char symbol, int nextPhraseIndex, int nextPhrase2Index)
+    private int _stateIndex;
+    public state(int index,char symbol, int nextPhraseIndex, int nextPhrase2Index)
     {
+        _stateIndex = index;
         _symbol = symbol;
         _nextPhrase1 = nextPhraseIndex;
         _nextPhrase2 = nextPhrase2Index;
@@ -16,8 +20,19 @@ public class state {
     {
         return this._nextPhrase1;
     }
+    public int nextPhraseIndex(int nextPhrase)
+    {
+        this._nextPhrase1 = nextPhrase;
+        return this._nextPhrase1;
+    }
     public int nextPhrase2Index()
     {
         return this._nextPhrase2;
     }
+    public int nextPhrase2Index(int nextPhrase2)
+    {
+        this._nextPhrase2 = nextPhrase2;
+        return this.nextPhrase2Index();
+    }
+    public int stateIndex(){return this._stateIndex;}
 }
