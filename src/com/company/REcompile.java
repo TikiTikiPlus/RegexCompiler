8a05e4761587ad11a0ab6433effe02f05b1e73c6
TikiTikiPlus <nootherthanme65@gmail.com>
2022-04-28 14:18:50 +1200

    done some shit

--- src/com/company/REcompile.java
+++ src/com/company/REcompile.java
@@ -215,6 +215,7 @@ public class REcompile {
                         FiniteStateMachine.add(escapeState);
                         stateInt++;
                         globalInt++;
+                        fsm = escapeState;
                     }
                 }
             } else {
